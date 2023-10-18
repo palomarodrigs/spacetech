df558e49e3ca4999a4e332b80d39a0758efc982c
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2023-10-17 23:12:45 -0300

    feat(header): add auth handler to header component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import {
   HomeIcon,
   ListOrderedIcon,
   LogInIcon,
+  LogOutIcon,
   MenuIcon,
   Percent,
   ShoppingCartIcon,
@@ -9,8 +12,26 @@ import {
 import { Button } from "./button";
 import { Card } from "./card";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Separator } from "./separator";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "./dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
 
 const Header = () => {
+  const { status, data } = useSession();
+
+  const handleLogin = async () => {
+    await signIn();
+  };
+
+  const handleLogout = async () => {
+    await signOut();
+  };
+
   return (
     <Card className="flex items-center justify-between p-[1.25rem]">
       <Sheet>
@@ -21,18 +42,58 @@ const Header = () => {
         </SheetTrigger>
 
         <SheetContent side="left">
-          <div className="mt-6 flex items-center justify-between">
+          <div className="mt-6 flex justify-between">
             <SheetHeader className="text-left text-lg font-semibold">
               Menu
             </SheetHeader>
 
-            <Button className="gap-2 text-left">
-              <LogInIcon size={16} />
-              Fazer login
-            </Button>
+            {status === "authenticated" && data?.user && (
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <div className="flex items-center gap-2">
+                    <div className="flex flex-col">
+                      <p className="text-sm font-medium">{data.user.name}</p>
+                      <p className="text-left text-xs opacity-75">
+                        Boas compras!
+                      </p>
+                    </div>
+
+                    <Avatar className="border border-gray-300">
+                      <AvatarFallback>
+                        {data.user.name?.[0].toUpperCase()}
+                      </AvatarFallback>
+
+                      {data.user.image && <AvatarImage src={data.user.image} />}
+                    </Avatar>
+                  </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="p-2">
+                  <Button
+                    onClick={handleLogout}
+                    className="gap-2 bg-gray-500 text-left text-xs hover:bg-gray-600"
+                  >
+                    <LogOutIcon size={14} />
+                    Fazer logout
+                  </Button>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            )}
+
+            {status === "unauthenticated" && (
+              <Button onClick={handleLogin} className="gap-2 text-left">
+                <LogInIcon size={16} />
+                Fazer login
+              </Button>
+            )}
+
+            {/* {status === "authenticated" && (
+            
+            )} */}
           </div>
 
-          <div className="mt-6 flex flex-col gap-3">
+          <Separator className="mb-6 mt-6" />
+
+          <div className="flex flex-col gap-3">
             <Button variant="outline" className="w-full justify-start gap-2">
               <HomeIcon size={16} />
               Início
