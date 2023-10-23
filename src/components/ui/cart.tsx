5acf04b6549e09c617ad0d93636ee4cacb7697db
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2023-10-23 15:54:08 -0300

    feat(cart): ensure cart products can be scrolled

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -8,83 +8,88 @@ import { Button } from "./button";
 import Link from "next/link";
 import { SheetClose } from "./sheet";
 import { Separator } from "./separator";
+import { ScrollArea } from "./scroll-area";
 
 const Cart = () => {
   const { products, subtotal, totalDiscount, total } = useContext(CartContext);
   return (
-    <>
+    <div className="flex h-full flex-col gap-8">
       {products.length > 0 ? (
-        <div className="flex flex-col gap-8">
-          <Badge
-            className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
-            variant="outline"
-          >
-            <ShoppingCartIcon size={16} />
-            Carrinho
-          </Badge>
+        <Badge
+          className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+          variant="outline"
+        >
+          <ShoppingCartIcon size={16} /> Carrinho
+        </Badge>
+      ) : null}
 
-          <div className="flex flex-col gap-5">
-            {products.map((product) => (
-              <CartItem
-                key={product.id}
-                product={computeProductTotalPrice(product as any) as any}
-              />
-            ))}
+      <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
+        <ScrollArea className="h-full">
+          <div className="flex h-full flex-col gap-8">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CartItem
+                  key={product.id}
+                  product={computeProductTotalPrice(product as any) as any}
+                />
+              ))
+            ) : (
+              <div className="mt-5 flex flex-col items-center gap-2">
+                <PackageOpenIcon size={35} />
+
+                <div className="flex flex-col items-center">
+                  <p className="opacity-75">
+                    Carrinho <span className="font-bold">vazio</span>.
+                  </p>
+
+                  <p>Vamos fazer compras?</p>
+                </div>
+
+                <SheetClose asChild>
+                  <Link href="/">
+                    <Button>Comprar agora</Button>
+                  </Link>
+                </SheetClose>
+              </div>
+            )}
+          </div>
+        </ScrollArea>
+      </div>
+
+      {products.length > 0 && (
+        <div className="flex flex-col gap-3">
+          <Separator />
+
+          <div className="flex items-center justify-between text-xs">
+            <p>Subtotal</p>
+            <p>R$ {subtotal.toFixed(2).replace(".", ",")}</p>
           </div>
 
-          <div className="flex flex-col gap-3">
-            <Separator />
+          <Separator />
 
-            <div className="flex items-center justify-between text-xs">
-              <p>Subtotal</p>
-              <p>R$ {subtotal.toFixed(2).replace(".", ",")}</p>
-            </div>
-
-            <Separator />
-
-            <div className="flex items-center justify-between text-xs">
-              <p>Entrega</p>
-              <p className="uppercase">Grátis</p>
-            </div>
-
-            <Separator />
-
-            <div className="flex items-center justify-between text-xs">
-              <p>Descontos</p>
-              <p className="uppercase">
-                - R$ {totalDiscount.toFixed(2).replace(".", ",")}
-              </p>
-            </div>
-
-            <Separator />
-
-            <div className="flex items-center justify-between text-sm font-bold">
-              <p>Total</p>
-              <p className="uppercase">
-                R$ {total.toFixed(2).replace(".", ",")}
-              </p>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className="mt-5 flex flex-col items-center gap-2">
-          <PackageOpenIcon size={35} />
-
-          <div className="flex flex-col items-center">
-            <p className="opacity-75">
-              Carrinho <span className="font-bold">vazio</span>.
-            </p>
-            <p>Vamos fazer compras?</p>
+          <div className="flex items-center justify-between text-xs">
+            <p>Entrega</p>
+            <p className="uppercase">Grátis</p>
           </div>
 
-          <SheetClose asChild>
-            <Link href="/">
-              <Button>Comprar agora</Button>
-            </Link>
-          </SheetClose>
+          <Separator />
+
+          <div className="flex items-center justify-between text-xs">
+            <p>Descontos</p>
+            <p>- R$ {totalDiscount.toFixed(2).replace(".", ",")}</p>
+          </div>
+
+          <Separator />
+
+          <div className="flex items-center justify-between text-sm font-bold">
+            <p>Total</p>
+            <p>R$ {total.toFixed(2).replace(".", ",")}</p>
+          </div>
+
+          <Button className="mt-7 font-bold uppercase">Finalizar compra</Button>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
