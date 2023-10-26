d6058e0333d0b61c2530ccda747013f3b175a916
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2023-10-26 10:33:12 -0300

    feat(orders): ensure user has completed orders

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,10 +1,16 @@
 import BadgeTitle from "@/components/ui/badge-title";
 import { authOptions } from "@/lib/auth";
 import { prismaClient } from "@/lib/prisma";
-import { PackageSearchIcon } from "lucide-react";
+import {
+  PackageSearchIcon,
+  PackageXIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
 import { getServerSession } from "next-auth";
 import OrderItem from "./components/order-item";
 import AccessDenied from "@/components/ui/access-denied";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
@@ -30,12 +36,37 @@ const OrderPage = async () => {
 
   return (
     <div className="mt-5 flex flex-col gap-8 p-5">
-      <BadgeTitle icon={<PackageSearchIcon size={16} />} title="Meus pedidos" />
+      {orders.length > 0 ? (
+        <BadgeTitle
+          icon={<PackageSearchIcon size={16} />}
+          title="Meus pedidos"
+        />
+      ) : null}
 
       <div className="flex flex-col gap-5">
-        {orders.map((order) => (
-          <OrderItem key={order.id} order={order} />
-        ))}
+        {orders.length > 0 ? (
+          orders.map((order) => <OrderItem key={order.id} order={order} />)
+        ) : (
+          <div className="mt-[14.5rem] flex flex-col items-center justify-center gap-2">
+            <PackageXIcon size={35} />
+
+            <div className="text-center">
+              <h2 className="text-lg font-bold">Sem pedidos finalizados!</h2>
+
+              <p className="opacity-75">
+                Você precisa adicionar produtos ao seu carrinho e finalizar o
+                pedido.
+              </p>
+            </div>
+
+            <Link href="/">
+              <Button className="mt-3 gap-2 text-left">
+                <ShoppingCartIcon size={16} />
+                Comprar agora
+              </Button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
