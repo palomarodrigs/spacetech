fbff968a163636e885f815ac2ea896abf870e0e1
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2023-10-31 12:03:37 -0300

    style(PromoBanner): remove padding to desktop

--- src/app/(home)/components/promo-banner.tsx
+++ src/app/(home)/components/promo-banner.tsx
@@ -5,7 +5,7 @@ const PromoBanner = ({ alt, ...props }: ImageProps) => {
     <Image
       width={350}
       height={150}
-      className="h-auto w-full px-5"
+      className="h-auto w-full px-5 lg:px-0"
       sizes="100vw"
       alt={alt}
       {...props}
