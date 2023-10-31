b904e146f367dd1599920326125457f6720f85ab
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2023-10-31 11:23:47 -0300

    feat(SectionTitle): add view all products link

--- src/components/ui/section-title.tsx
+++ src/components/ui/section-title.tsx
@@ -1,7 +1,23 @@
+import { ArrowRightIcon } from "lucide-react";
+import Link from "next/link";
 import { ComponentProps } from "react";
 
-const SectionTitle = ({ children, ...props }: ComponentProps<"p">) => {
-  return <p className="mb-5 pl-5 font-bold uppercase">{children}</p>;
+interface SectionTitleProps extends ComponentProps<"p"> {
+  url: string;
+}
+
+const SectionTitle = ({ children, url, ...props }: SectionTitleProps) => {
+  return (
+    <div className="mb-5 flex items-center justify-between px-5">
+      <p className="font-bold uppercase">{children}</p>
+
+      <Link href={url}>
+        <span className="flex cursor-pointer items-center gap-1 text-sm font-semibold transition duration-300 ease-in-out hover:opacity-80">
+          Ver todos <ArrowRightIcon size={16} />
+        </span>
+      </Link>
+    </div>
+  );
 };
 
 export default SectionTitle;
