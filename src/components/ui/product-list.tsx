1d6b8721999409e9c15d60a73989aa86cce6aa26
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2023-11-02 18:00:53 -0300

    chore(ProductList): add limit products view based on screen size and change padding to desktop

--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -1,16 +1,48 @@
+"use client";
+
 import ProductItem from "@/components/ui/product-item";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { Product } from "@prisma/client";
+import { useEffect, useState } from "react";
 
 interface ProductListProps {
   products: Product[];
 }
 
 const ProductList = ({ products }: ProductListProps) => {
+  const [isDesktop, setIsDesktop] = useState<boolean>(false);
+  const [visibleProducts, setVisibleProducts] = useState<Product[]>([]);
+  const productViewLimit = 5;
+
+  useEffect(() => {
+    function handleResize() {
+      setIsDesktop(window.innerWidth > 1024);
+    }
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isDesktop) {
+      setVisibleProducts(products.slice(0, productViewLimit));
+    } else {
+      setVisibleProducts(products);
+    }
+  }, [isDesktop, products, productViewLimit]);
+
   return (
-    <div className="flex w-full gap-4 overflow-x-auto px-5 lg:max-w-[1262px] lg:gap-[39px] xl:max-w-none [&::-webkit-scrollbar]:hidden">
-      {products.map((product) => (
-        <div key={product.id} className="w-[170px] max-w-[170px]">
+    <div className="flex w-full gap-4 overflow-x-auto px-5 lg:gap-[2.55rem] lg:px-[100px] [&::-webkit-scrollbar]:hidden">
+      {visibleProducts.map((product) => (
+        <div
+          key={product.id}
+          className="w-[170px] max-w-[170px] lg:w-[180px] lg:max-w-[180px]"
+        >
           <ProductItem product={computeProductTotalPrice(product)} />
         </div>
       ))}
