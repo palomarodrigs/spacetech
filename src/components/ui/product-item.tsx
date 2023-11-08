7d4dfe4d61411477492df355e3c557333a313d3b
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2023-11-08 19:12:01 -0300

    style(ProductItem): change border color on dark mode

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -10,7 +10,7 @@ interface ProductItemProps {
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
     <Link href={`/product/${product.slug}`}>
-      <div className="flex flex-col gap-4 rounded-lg border border-gray-200 p-3 lg:w-[180px]">
+      <div className="flex flex-col gap-4 rounded-lg border border-gray-200 p-3 dark:border-zinc-700 lg:w-[180px]">
         <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent lg:h-[190px]">
           <Image
             src={product.imageUrls[0]}
