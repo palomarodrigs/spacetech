6aadabaa9872280bb905f1eb6b2c59b405a889c7
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2023-11-20 21:10:31 -0300

    refactor(authOptions): update GoogleProvider profile handling and improve callbacks for JWT and session.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,22 +4,48 @@ import { AuthOptions } from "next-auth";
 import { prismaClient } from "./prisma";
 
 export const authOptions: AuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+
   adapter: PrismaAdapter(prismaClient),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: `${profile.given_name} ${profile.family_name}`,
+          email: profile.email,
+          image: profile.picture,
+          role: profile.role ? profile.role : "user",
+        };
+      },
     }),
   ],
   callbacks: {
-    async session({ session, token, user }) {
-      session.user = { ...session.user, id: user.id } as {
-        id: string;
-        name: string;
-        email: string;
-      };
+    jwt: async ({ token, user }) => {
+      const customUser = user as unknown as any;
 
-      return session;
+      if (user) {
+        return {
+          ...token,
+          role: customUser.role,
+        };
+      }
+
+      return token;
+    },
+    session: async ({ session, token }) => {
+      return {
+        ...session,
+        user: {
+          name: token.name,
+          email: token.email,
+          role: token.role,
+        },
+      };
     },
   },
 };
