32ba64611db53253cb099a385f5f3371d3dec3e0
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2023-11-23 18:50:21 -0300

    feat(admin): add layout

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,42 @@
+import "../globals.css";
+import "react-toastify/dist/ReactToastify.min.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import { AuthProvider } from "@/providers/auth";
+import { ToastContainer } from "react-toastify";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Aqui tem tudo que o seu setup precisa!",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="flex h-full flex-col">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <AuthProvider>
+              <div className="flex overflow-hidden">
+                <ToastContainer theme="colored" />
+                {children}
+              </div>
+            </AuthProvider>
+          </ThemeProvider>
+        </div>
+      </body>
+    </html>
+  );
+}
