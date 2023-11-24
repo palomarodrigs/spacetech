eb97bb59d06e934674c1c34d47d93236c0ecf3bf
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2023-11-24 19:17:24 -0300

    feat(dashboard): add TopSide and CardsSummary components

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,5 +1,17 @@
+import { Separator } from "@/components/ui/separator";
+import TopSide from "./components/topside";
+import CardsSummary from "./components/cards-summary";
+
 const DashboardPage = () => {
-  return <h1>Dash</h1>;
+  return (
+    <main className="w-full lg:p-8">
+      <TopSide />
+
+      <Separator className="lg:mb-6 lg:mt-6" />
+
+      <CardsSummary />
+    </main>
+  );
 };
 
 export default DashboardPage;
