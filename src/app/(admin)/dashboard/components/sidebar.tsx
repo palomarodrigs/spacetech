97cd8dd483660afc75dc5c3d1b132846b547ab40
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2023-12-01 17:09:26 -0300

    chore(sidebar): add navigation

--- src/app/(admin)/dashboard/components/sidebar.tsx
+++ src/app/(admin)/dashboard/components/sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Logo from "@/components/ui/logo";
 import { ModeToggle } from "@/components/ui/mode-toggle";
@@ -8,9 +10,10 @@ import {
   PackageSearchIcon,
 } from "lucide-react";
 import Link from "next/link";
-
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const path = usePathname();
   return (
     <main className="hidden min-w-[300px] flex-col gap-8 border-r border-solid border-accent p-8 lg:flex">
       <header className="flex items-center justify-between">
@@ -21,26 +24,60 @@ const Sidebar = () => {
 
       <div className="flex flex-col gap-3">
         <Link href="/dashboard">
-          <Button variant="outline" className="w-full justify-start gap-2">
+          <Button
+            variant="outline"
+            className={`link w-full justify-start gap-2 ${
+              path === "/dashboard"
+                ? "active bg-primary text-white hover:bg-primary hover:text-white"
+                : ""
+            }`}
+          >
             <LayoutDashboardIcon size={16} />
             Dashboard
           </Button>
         </Link>
 
-        <Button variant="outline" className="w-full justify-start gap-2">
-          <PackageIcon size={16} />
-          Produtos
-        </Button>
+        <Link href="/dashboard/products">
+          <Button
+            variant="outline"
+            className={`link w-full justify-start gap-2 ${
+              path === "/dashboard/products"
+                ? "active bg-primary text-white hover:bg-primary hover:text-white"
+                : ""
+            }`}
+          >
+            <PackageIcon size={16} />
+            Produtos
+          </Button>
+        </Link>
 
-        <Button variant="outline" className="w-full justify-start gap-2">
-          <ListOrderedIcon size={16} />
-          Categorias
-        </Button>
+        <Link href="/dashboard/categories">
+          <Button
+            variant="outline"
+            className={`link w-full justify-start gap-2 ${
+              path === "/dashboard/categories"
+                ? "active bg-primary text-white hover:bg-primary hover:text-white"
+                : ""
+            }`}
+          >
+            <ListOrderedIcon size={16} />
+            Categorias
+          </Button>
+        </Link>
 
-        <Button variant="outline" className="w-full justify-start gap-2">
-          <PackageSearchIcon size={16} />
-          Pedidos
-        </Button>
+        <Link href="/dashboard/orders">
+          <Button
+            variant="outline"
+            className={`link w-full justify-start gap-2 ${
+              path === "/dashboard/orders"
+                ? "active bg-primary text-white hover:bg-primary hover:text-white"
+                : ""
+            }`}
+          >
+            <PackageSearchIcon size={16} />
+            Pedidos
+          </Button>
+        </Link>
       </div>
     </main>
   );
